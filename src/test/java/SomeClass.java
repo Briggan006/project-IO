0cf91d6ec463dfde0d96eebace9afe72734e3570
m4rcinek <marcin.briggan@gmail.com>
2025-06-10 18:36:41 +0200

    stacja 7 -mam dosc

---
+++ src/test/java/SomeClass.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class SomeClass {
+
+
+    @Test
+    public void test1(){
+        int value = 2;
+        int value2 = 3;
+        assert value + value2 == 5;
+    }
+
+    @Test
+    public void test2(){
+        int y = 1;
+        int x = 2;
+        assert x-y == 1;
+    }
+}
